b892fa25223ba783331fa4ac35b6680a470c9bf2
epthed <epthedemail@gmail.com>
2021-03-24 00:13:36 -0700

    force ssl

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import App from './App.vue'
 import SocketIO from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io';
 import store from './vuex-store.js'
+import sslRedirect from "heroku-ssl-redirect";
 
-
+//todo graceful handling of sigterm
 let api = ""
 if (typeof process.env.VUE_APP_API_URL !== 'undefined'){
     api = process.env.VUE_APP_API_URL.toString();
+    Vue.use(sslRedirect()); // if we're in heroku, force HTTPS
 }else {
     api = "http://localhost:5000"; // nodejs ENVs and heroku ENVs seem mutually exclusive,
-    // removing fileENV and hardcoding the local case
+    // removing file.ENV and hardcoding the local case
 }
 export const SocketInstance = new SocketIO(api);
 
